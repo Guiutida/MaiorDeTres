548a492ea1abbd457b959a88e26cab1154968d63
Guilherme Utida De Jeses <guiutida@gmail.com>
2025-04-26 01:36:40 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int a, b, c, maior;
+
+Console.Clear();
+Console.Write("Escreva o primeiro número: ");
+a = Convert.ToInt16(Console.ReadLine());
+
+Console.Write("Escreva o segundo número: ");
+b = Convert.ToInt16(Console.ReadLine());
+
+Console.Write("Escreva o terceiro número: ");
+c = Convert.ToInt16(Console.ReadLine());
+ 
+
+if (a >= b && a >= c)
+    maior = a;
+else if (b >= a && b >= c)
+    maior = b;
+else
+    maior = c;
+
+Console.WriteLine("o maior numero é: " + maior);
